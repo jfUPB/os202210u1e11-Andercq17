ac4ea129092d8d61121f149f95a0e67d68ce76ed
andersson <a.cardenas.quiros@gmail.com>
2022-02-11 11:02:33 -0500

    primera version

--- problem1/eventList.c
+++ problem1/eventList.c
@@ -5,7 +5,7 @@
 
 EventList *CreateEventList(void)
 {
-    EventList *eventList = NULL;
+    EventList *eventList = malloc(sizeof(EventList));
 
     return eventList;
 }
@@ -26,7 +26,25 @@ Event *SearchEvent(EventList *this, char *name)
 
 void AddEvent(EventList *this, Event *event)
 {
+    Event *r = this->head;
+    if (this->isEmpty != 0)
+    {
+        while (r != NULL)
+        {
+            if (*(r->eventName + 2) == *(event->eventName + 2) && *(r->eventName + 3) == *(event->eventName + 3))
+                return;
 
+            r = r->next;
+        }
+        this->last->next = event;
+        this->last = event;
+    }
+    else
+    {
+        this->head = event;
+        this->last = event;
+        this->isEmpty = 1;
+    }
 }
 
 void RemoveEvent(EventList *this, char *name)
@@ -35,6 +53,6 @@ void RemoveEvent(EventList *this, char *name)
 
 void ListEvents(EventList *this)
 {
-    // imprime esto si la lista está vacía
+    
     printf("empty\n");
 }
