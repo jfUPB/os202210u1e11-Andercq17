0e58442335537a7a071a3a5238ce161647d59ea9
andersson <a.cardenas.quiros@gmail.com>
2022-02-13 22:08:28 -0500

    ultima version y completa

--- problem1/event.c
+++ problem1/event.c
@@ -2,19 +2,20 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 Event *CreateEvent(char *name)
 {
-    Event *event = malloc(sizeof(Event));
-    sscanf(name,"%s", event->eventName);
-    event->next=NULL;
-    return event;
+    Event *evento = malloc(sizeof(Event));
+    evento->next = NULL;
+    name[15]=0;
+    sscanf(name,"%s",evento->eventName);
+    return evento;
 }
 
 void DestroyEvent(Event *this)
 {
     free(this);
 }
-
 
 
 
